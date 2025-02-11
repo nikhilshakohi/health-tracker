7e8052779a451ea3221ee81577a70e3f47dae8f9
NIKHIL SHAKOHI <nikhilshakohi@gmail.com>
2025-02-11 07:39:17 +0000

    feat(init): project setup

---
+++ src/main/java/com/nikhil/healthtracker/TabletTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.healthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikhil/healthtracker/TabletTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.healthtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
